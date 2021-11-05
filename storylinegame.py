1c4c92cd99a36b6de603862cb3cabdb493970297
rubenwkossen <32533@ma-web.nl>
2021-11-05 11:44:12 +0100

    first

--- storylinegame.py
+++ storylinegame.py
@@ -29,18 +29,63 @@ if answer == "yes":
                             print("you go to asia and live a life of misery")
                         elif answer == "africa":
                             print("you die")                 
-                        elif answer == "stay":
-                            print("the army is infront or your door to take you to the army, (army/run)")
-                            answer = input ("army/run")
-                            if answer == "run":
-                                print("you run away to turkeye")
-                            elif answer == "army":
-                                print(" you go to the army where you train and sent to war, to you go to war or stay at the camp")
-                                answer = input ("(stay/war)")
-                                if answer == "stay":
-                                    print("death")
-                                elif answer == "war":
-                                    print("death")  
+    elif answer == "stay":
+        print("the army is infront or your door to take you to the army, (army/run)")
+        answer = input ("army/run")
+        if answer == "run":
+            print("you run away to turkeye")
+            print("you run to turkeye, there you run to the borders where you get the choice to get on the boat or land")
+        answer = input ("(land/boat)")
+        if answer == "land":
+            print("you run to, (Bulgarije/greece)")
+            answer = input ("(Bulgarije/greece)")
+            if answer == "greece":
+                print("you are send to the netherlands, their give you a job, what is it, (fisher/teacher)")
+                answer = input ("(fisher/teacher)")
+                if answer == "teacher":
+                    print("you findt love, and make a family")
+                elif answer == "fisher":
+                    print("you findt love, and make a family")
+                elif answer == "Bulgarije":
+                    print("you are send to the netherlands, their give you a job, what is it, (fisher/teacher) ")
+                    answer = input ("(fisher/teacher)")
+                    if answer == "teacher":
+                        print("you findt love, and make a family")
+                    elif answer == "fisher":
+                        print("you findt love, and make a family")  
+                    elif answer == "boat":
+                        print("their take you in as a asiel locker, where you get a choose asia or africa")
+                        answer = input ("go to asia or africa")
+                        if answer == "asia":
+                            print("you go to asia and live a life of misery")
+                        elif answer == "africa":
+                            print("you die")                 
+        elif answer == "army":
+            print(" you go to the army where you train and sent to war, to you go to war or stay at the camp")
+            answer = input ("(stay/war)")
+            if answer == "stay":
+                print("death")
+            elif answer == "war":
+                print("death")  
 elif answer == "no":
     print("do you live in syrie")
     answer = input ("(yes/no)")
+    if answer == "yes":
+        print("you story begins at a airport with your family, where you want to take the plane to russia")
+        print("the securety can only take you family or you what do you choose")
+        answer = input ("(go/stay)")
+        if answer == "stay":
+            print("you stay behind and travel to russia alone")
+            print("you look around the airport if there is a other way to russia")
+            print("they have a fleat to Georgië")
+            print("do you take it")
+            print("or do you look for a other way")
+            answer = input ("this part is not ready yet")
+               
+            
+        elif answer == "go":
+            print("you leave you family behind, so you dont have a family aniemor,")
+            print("good ending")
+        
+elif answer == "no":
+    print("say yes your mother fucker")
